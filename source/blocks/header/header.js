2cd81005b82e5f1a9ca5f9d026703eb23209d8a4
GeorgeSemenov <zhora.sem@yandex.ru>
2022-01-19 11:48:44 +0300

    add sandwich

--- source/blocks/header/header.js
+++ source/blocks/header/header.js
@@ -1,2 +1,5 @@
 $(document).ready(function(){
+  $('.header__sandwich-ico-container').click(function(){
+    $('.header').toggleClass('header_expanded');
+  });
 })
--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -1,3 +1,4 @@
 import './index.scss';
 import '../../JS-components/libs.js';
+import '../../blocks/header/header.js';
 // import '../../vendor/nice-select/jquery-nice-select-1.1.0/js/jquery.nice-select.min.js';
